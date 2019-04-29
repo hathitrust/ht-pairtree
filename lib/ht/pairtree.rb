3521f16939c2b724f79185faacb18b24cd631cd0
Bill Dueber <bill@dueber.com>
2019-04-29 12:35:08 -0400

    Initial commit

---
+++ lib/ht/pairtree.rb
@@ -0,0 +1,9 @@
+require "ht/pairtree/version"
+
+module HathiTrust
+  # A simple PairTree implementation to make it easier to work with
+  # the HathiTrust pairtree structure.
+ class PairTree
+    class Error < StandardError; end
+  end
+end
---
+++ lib/ht/pairtree/version.rb
@@ -0,0 +1,5 @@
+module HathiTrust
+ class PairTree
+    VERSION = "0.1.0"
+  end
+end
